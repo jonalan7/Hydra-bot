5d6e3eda8e5f11b98a30bfdbab38fe54569b1997
Jónalan de Lima <jonalan7@gmail.com>
2022-05-18 01:12:27 -0300

    fix: neededStore

--- src/assets/help/inject-paresite.js
+++ src/assets/help/inject-paresite.js
@@ -30,7 +30,7 @@ export const injectParasiteSnake = async () => {
             }
 
             let neededStore = filterObjects.find((e) => e.type === 'Module');
-            
+
             if (neededStore) {
                 window.Store = filterObjects.yesModule ? filterObjects.yesModule : {};
             }
@@ -38,19 +38,21 @@ export const injectParasiteSnake = async () => {
             filterObjects.splice(filterObjects.indexOf(neededStore), 1);
 
             filterObjects.forEach((needObj) => {
-                if (needObj.yesModule) {  
+                if (needObj.yesModule) {
                     window.Store[needObj.type] = needObj.yesModule;
                 }
             });
 
-            Object.keys(neededStore.yesModule).forEach((e) => { 
-                window.Store[e] = neededStore.yesModule[e];
-            })
-            
+            if (typeof neededStore?.yesModule === 'object') {
+                Object.keys(neededStore.yesModule).forEach((e) => {
+                    window.Store[e] = neededStore.yesModule[e];
+                })
+            }
+
             if (Store && Store.BusinessProfile) {
                 Store.Chat._findAndParse = Store.BusinessProfile._findAndParse;
                 Store.Chat._find = Store.BusinessProfile._find;
-              }
+            }
         },
     ]);
 }
