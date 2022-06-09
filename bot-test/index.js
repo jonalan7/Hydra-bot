5ae06fe391b86238aabdfb0fe742007190e73b30
Jónalan de Lima <jonalan7@gmail.com>
2022-06-08 23:42:31 -0300

    feat: doc Administration

--- bot-test/index.js
+++ bot-test/index.js
@@ -2,61 +2,70 @@ const hydraBot = require('../dist');
 
 (async () => {
 
-    hydraBot.initWs({
+    // hydraBot.initWs({
+    //     puppeteerOptions: {
+    //         headless: false,
+    //     }
+    // });
+
+
+    let client;
+    // start bot service
+    const webpack = await hydraBot.initServer({
         puppeteerOptions: {
             headless: false,
         }
     });
 
-    // start bot service
-    // const webpack = await hydraBot.initServer({
-    //     puppeteerOptions: {
-    //         headless: false,
-    //         executablePath: '',
-    //     }
-    // });
+    // return to current whatsapp interface
+    webpack.on('interfaceChange', (change) => {
+        console.log("interfaceChange: ", change);
+    });
 
-    // // return to current whatsapp interface
-    // webpack.on('interfaceChange', (change) => {
-    //     console.log("interfaceChange: ", change);
-    // });
+    // return qrcode parameters
+    webpack.on('qrcode', (qrcode) => {
+        console.log('qrcode: ', qrcode);
+    });
 
-    // // return qrcode parameters
-    // webpack.on('qrcode', (qrcode) => {
-    //     console.log('qrcode: ', qrcode);
-    // });
+    // return connection information
+    webpack.on('connection', async (conn) => {
 
-    // // return connection information
-    // webpack.on('connection', async (conn) => {
-    //     console.log(conn);
-    //     // if (conn) {
-    //     //     // send a text message
-    //     //     await webpack.sendMessage({
-    //     //         to: "557592641325@c.us",
-    //     //         body: "Oi eu sou um bot",
-    //     //         options: {
-    //     //             type: 'text',
-    //     //         }
-    //     //     }).then((result) => {
-    //     //         console.log(result);
-    //     //     }).catch((error) => {
-    //     //         console.log(error);
-    //     //     });
-    //     // }
-    // });
+        // browser information!
+        if (conn.statusFind === 'browser') {
+            console.log('info Browser: ', conn.text);
+        }
 
-    // // return receive new messages
-    // webpack.on('newMessage', (newMsg) => {
-    //     // when is received
-    //     if (!newMsg.isSentByMe) {
-    //         // message received!
-    //         console.log('NewMessageReceived: ', newMsg);
-    //     }
-    //     // when is it sent
-    //     if (!!newMsg.isSentByMe) {
-    //         // Message sent
-    //         console.log('NewMessageSent: ', newMsg);
-    //     }
-    // });
+        // Was connected to whatsapp chat
+        if (conn.connect) {
+            // send a text message
+            client = conn.client;
+            await client.sendMessage({
+                to: "0000000000@c.us",
+                body: "Oi eu sou um bot",
+                options: {
+                    type: 'text',
+                }
+            }).then((result) => {
+                console.log(result);
+            }).catch((error) => {
+                console.log(error);
+            });
+
+        }
+    });
+
+    // return receive new messages
+    webpack.on('newMessage', async (newMsg) => {
+        // when is received
+        if (!newMsg.result.isSentByMe) {
+            // message received!
+            console.log('NewMessageReceived: ', newMsg.result);
+        }
+        // when is it sent
+        if (!!newMsg.result.isSentByMe) {
+            // Message sent
+            console.log('NewMessageSent: ', newMsg.result);
+        }
+    });
 
 })();
